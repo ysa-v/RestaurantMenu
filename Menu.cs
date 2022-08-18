7ac695e0344d76d30aed763327c8cb4d1ffcb3e9
ysa-v <97559097+ysa-v@users.noreply.github.com>
2022-08-17 20:31:32 -0500

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MenuStudio
+{
+    public class Menu
+    {
+        Dictionary<double,string> ItemsPriceList { get; set; }
+        List<object> FullMenu { get; set; }
+        List<string> CategoryOrder { get; set; }
+        DateTime MenuLastUpdated { get; set; }
+        int NumberOfNewItems { get; set; }
+
+        public Menu(List<object> fullMenu)
+        {
+            FullMenu = fullMenu;
+        }
+    }
+}
+
---
+++ MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic
+namespace MenuStudio
+{
+    public class MenuItems
+    {
+        double Price { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        string Category { get; set; }
+        bool MarkedAsNew { get; set; }
+        double Popularity { get; set; }
+        DateTime ItemCreated { get; set; }
+        DateTime ItemUpdated { get; set; }
+
+        public MenuItems(double price, string title, string description, bool markedAsNew)
+        {
+            Price = price;
+            Title = title;
+            Description = description;
+            MarkedAsNew = markedAsNew;
+        }
+
+        public MenuItems()
+    }
+}
+
